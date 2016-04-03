94be1ddd94d08890a11895d04fee3c008a2b4a86
Christian Dinh <Christian Dinh>
2016-04-03 18:24:14 -0500

    Remove unused imports

--- app/src/main/java/cs354/plunjr/RestroomListActivity.java
+++ app/src/main/java/cs354/plunjr/RestroomListActivity.java
@@ -1,17 +1,12 @@
 package cs354.plunjr;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.RatingBar;
