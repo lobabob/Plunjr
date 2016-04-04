6517aaa856adc363d7ebaa209b5ba7ed23dd4bae
Farhankhan Daya <lobabob@users.noreply.github.com>
2016-04-04 17:11:54 -0500

    Resolved #19

--- app/src/main/java/cs354/plunjr/PlunjrAPIClient.java
+++ app/src/main/java/cs354/plunjr/PlunjrAPIClient.java
@@ -8,11 +8,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 
 /**
@@ -57,6 +62,42 @@ public class PlunjrAPIClient {
         return arr;
     }
 
+    public JSONArray postReview(Context context, int restroomID, Map<String, Object> params) {
+        String res = "";
+        StringBuilder postData = new StringBuilder();
+        JSONArray arr = new JSONArray();
+
+        // Transform parameters into a correctly formatted string
+        try {
+            for (Map.Entry<String, Object> e : params.entrySet()) {
+                postData.append(URLEncoder.encode(String.valueOf(e.getKey()), "utf-8"));
+                postData.append("=");
+                postData.append(URLEncoder.encode(String.valueOf(e.getValue()), "utf-8"));
+                postData.append("&");
+            }
+            postData.setLength(postData.length() - 1);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+        }
+
+        // Make POST request to API
+        try {
+            res = new PlunjrGet().execute(String.format(context.getString(R.string.post_review_uri),
+                    restroomID), postData.toString()).get();
+        } catch(Exception e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+        }
+
+        // Convert response to JSON array
+        try {
+            arr = new JSONArray(res);
+        } catch(JSONException e) {
+            Log.e(LOG_TAG, e.getMessage(), e);
+        }
+
+        return arr;
+    }
+
     private class PlunjrGet extends AsyncTask<String, Void, String> {
 
         @Override
@@ -77,16 +118,66 @@ public class PlunjrAPIClient {
                 // Open connection and prepare to receive response
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuffer buffer = new StringBuffer("");
+                StringBuilder build = new StringBuilder();
                 String line;
 
                 // Read response into buffer
                 while((line = reader.readLine()) != null) {
-                    buffer.append(line);
+                    build.append(line);
                 }
                 reader.close();
                 connection.disconnect();
-                res = buffer.toString();
+                res = build.toString();
+            } catch(IOException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+            }
+            return res;
+        }
+    }
+
+    private class PlunjrPost extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String res = "[]";
+            try {
+                URL url = new URL(params[0]);
+                res = post(url, params[1]);
+            } catch(MalformedURLException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+            }
+            return res;
+        }
+
+        private String post(URL url, String postData) {
+            String res = "[]";
+            try {
+                // Open connection and prepare to send postData
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                connection.setRequestProperty("charset", "utf-8");
+                connection.setRequestProperty("Content-Length", Integer.toString(postData.length()));
+                connection.setUseCaches(false);
+
+                // Send postData
+                DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+                wr.write(postData.getBytes("utf-8"));
+                wr.close();
+
+                // Receive Response
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder build = new StringBuilder();
+                String line;
+
+                // Read response into buffer
+                while ((line = reader.readLine()) != null) {
+                    build.append(line);
+                }
+                reader.close();
+                connection.disconnect();
+                res = build.toString();
             } catch(IOException e) {
                 Log.e(LOG_TAG, e.getMessage(), e);
             }
